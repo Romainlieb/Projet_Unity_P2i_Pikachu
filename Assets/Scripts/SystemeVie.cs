a0cd28270e94b1f921e1bde662c307f91196596c
dragoslup1976 <dragoslup1976@gmail.com>
2026-01-09 09:28:46 +0100

    Je fait le lien entre SecondMenu-UI/Game Over et le code.
    Après avoir perdu les 3 vies, l'écran « game over » s'affiche, et en cliquant sur le bouton « play », vous recommencez le jeu.

--- Assets/Scripts/SystemeVie.cs
+++ Assets/Scripts/SystemeVie.cs
@@ -1,26 +1,32 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SystemeVie : MonoBehaviour
 {
     public int maxHealth = 3;
     int currentHealth;
 
-    // read-only property (tutorial requirement)
+    // read-only property
     public int health { get { return currentHealth; } }
+
+    [Header("Game Over")]
+    public string gameOverSceneName = "SecondMenu-UI";
 
     void Start()
     {
-        currentHealth = maxHealth;   // start game with full health
+        currentHealth = maxHealth;  
         Debug.Log(currentHealth + "/" + maxHealth);
-    }
-
-    void Update()
-    {
     }
 
     public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+
+        // GAME OVER
+        if (currentHealth <= 0)
+        {
+            SceneManager.LoadScene(gameOverSceneName);
+        }
     }
 }
